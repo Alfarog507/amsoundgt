17b82dec0335d033fe1c373a7d65d89067201af7
Alfarog507 <alfarog507@gmail.com>
2025-02-17 20:35:07 -0600

    Agregamos la funcionalidad HEAD para mejorar el SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const font = Poppins({
   weight: ["100", "300", "400", "500", "600", "700", "200", "900", "800"],
@@ -20,6 +21,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Artículos y accesorios musicales de las mejores marcas al mejor precio"
+        />
+        <meta property="og:title" content="AM Sound GT" />
+        <meta
+          property="og:description"
+          content="Artículos y accesorios musicales de las mejores marcas al mejor precio"
+        />
+        <meta property="og:image" content="/images/og-image.jpg" />
+        <meta property="og:url" content="https://amsoundgt.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="AM Sound GT" />
+        <meta
+          name="twitter:description"
+          content="Artículos y accesorios musicales de las mejores marcas al mejor precio"
+        />
+        <meta name="twitter:image" content="/images/og-image.jpg" />
+        <title>AM Sound GT</title>
+      </Head>
       <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
